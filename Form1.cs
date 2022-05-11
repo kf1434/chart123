2deb15bce5ec1550b0aa92fd756320c915ef3493
kim tae-wan <105286631+kf1434@users.noreply.github.com>
2022-05-11 12:07:46 +0900

    visual studio c sharp winform chart
    kf1434@kakao.com

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chart123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Add(40);
+            chart1.Series[0].Points.Add(30);
+            chart1.Series[0].Points.Add(60);
+            chart1.Series[0].Points.Add(80);
+
+        }
+    }
+}
